b0279dc2bebe152260b5c0faf3420d8a80487d80
Kamil <kam.popczyk@gmail.com>
2018-06-18 21:55:36 +0200

    Done

--- sprint-planning-helper.py
+++ sprint-planning-helper.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import time
 
 
-class Task:  # define task with parameters
+class Task:
+    """Class Task store info about task"""
     def __init__(self, task_id, story_points, KSP):
         self.task_id = task_id
         self.story_points = story_points
@@ -11,19 +13,21 @@ class Task:  # define task with parameters
 
 
 class SprintPlanningHelper:
+    """SprintPlanningHelper class"""
     def __init__(self):
-        self.TaskList = []
+        self.task_list = []
 
-    def load_csv_file(self, filename):
-        if os.path.isfile(filename):  # exist ?
-            with open(filename, "r") as fileData:
+    def load_csv_file(self, file_name):
+        """Load csv file"""
+        if os.path.isfile(file_name):  # exist ?
+            with open(file_name, "r") as fileData:
                 fileData.__next__()  # skip header
-                for dataLine in fileData:
-                    dataLine = dataLine.replace("\n", "")  # new line?  I don't need that
-                    currentline = dataLine.split(",")
-                    if (len(currentline) == 3):  # correct data
-                        task = Task(int(currentline[0]), int(currentline[1]), int(currentline[2]))
-                        self.TaskList.append(task)
+                for data_line in fileData:
+                    data_line = data_line.replace("\n", "")  # new line?  I don't need that
+                    current_line = data_line.split(",")
+                    if (len(current_line) == 3):  # correct data
+                        task = Task(int(current_line[0]), int(current_line[1]), int(current_line[2]))
+                        self.task_list.append(task)
                     else:
                         print("Too few data in file.")
                         return False
@@ -31,16 +35,18 @@ class SprintPlanningHelper:
             print("File don't exist.")
             return False
 
-        return self.TaskList
+        return self.task_list
 
     def find_tasks_aproximation(self, velocity):  #  velocity >= the sum of story points
+        """Aproximation algorithm"""
         found_tasks = []
         actual_sum_of_story_points = 0
         # count value , value = KSP / story_points
-        for task in self.TaskList:
+        # value represents how many KSP per one story point
+        for task in self.task_list:
             task.value = task.KSP / task.story_points
         # sort them
-        task_list_copy = self.TaskList
+        task_list_copy = self.task_list
         task_list_copy.sort(key=lambda Task: Task.value, reverse=True)
         # let's find tasks
         for task in task_list_copy:
@@ -53,25 +59,28 @@ class SprintPlanningHelper:
         return found_tasks
 
 
-
-
-
 def main():
+
     arguments = sys.argv
 
-    # tasklist = loadCSVFile("csv_with_task.csv")
-    helper = SprintPlanningHelper()
-    helper.load_csv_file("csv_with_task.csv")
-    tasklist = helper.find_tasks_aproximation(13)
-
-    for t in tasklist:
-        print(t.task_id)
-
-    if len(arguments) == 0:
-        print("No arguments ! ")
+    if len(arguments) < 3:
+        print("Not enough arguments ! ")
 
     else:
-        print("Arguments ok")
+        helper = SprintPlanningHelper()
+        if helper.load_csv_file(arguments[1]) is False:
+            print("Error")
+        else:
+            task_list = helper.find_tasks_aproximation(int(arguments[2]))
+            task_list.sort(key=lambda Task: Task.task_id, reverse=False)
+            i = 0
+            for task in task_list:
+                sys.stdout.write(str(task.task_id))
+                if i < len(task_list) - 1:
+                    sys.stdout.write(", ")
+                i += 1
+
+            sys.stdout.write("\n")
 
 if __name__ == '__main__':
     main()
